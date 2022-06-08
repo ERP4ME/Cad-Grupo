d5ba23fb10fbc687b5efede20dbb84d3530760e5
ERP4ME <erp4me.automatizado@gmail.com>
2022-06-08 19:47:03 -0300

    refatorar

--- cypress/support/api/request.js
+++ cypress/support/api/request.js
@@ -3,7 +3,7 @@ class Request {
     getGrupo() {
         return cy.request({
             method: 'POST',
-            url: 'https://erpforme-hml.alterdata.com.br/koopon-produto-rest-api/grupos',
+            url: '/koopon-produto-rest-api/grupos',
             body: {
                 "propsPendenciasLista": [],
                 "descricao": "Ferramentas",
@@ -20,14 +20,11 @@ class Request {
 
         return cy.request({
             method: 'DELETE',
-            url: `https://erpforme-hml.alterdata.com.br/koopon-produto-rest-api/grupos/${id}`,
+            url: `/koopon-produto-rest-api/grupos/${id}`,
 
             failOnStatusCode: false
 
         })
-
-
-
 
     }
 
@@ -36,7 +33,7 @@ class Request {
             method: 'POST',
             url: 'koopon-financeiro-rest-api/commons/cartoes_operadora/',
             body: {
-                
+
                 "descricao": "Inter",
                 "cnpj": "81.552.500/0001-38",
 
@@ -48,25 +45,17 @@ class Request {
     }
 
     DeleteGrupoMovimentada(response) {
-        
+
 
         return cy.request({
             method: 'DELETE',
-            url: `https://erpforme-hml.alterdata.com.br/koopon-produto-rest-api/grupos/6`,
+            url: `/koopon-produto-rest-api/grupos/6`,
 
             failOnStatusCode: false
 
         })
 
-
-
-
     }
-
-
-
-    
-
 
 
 }
